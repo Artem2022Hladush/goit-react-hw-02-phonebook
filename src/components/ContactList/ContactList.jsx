c1cd61b03c63c0733c93fd8fbe51a3684247e328
Артем Гладуш <m1hommage5008@gmail.com>
2022-12-01 17:54:30 +0200

    add button

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,6 +5,7 @@ return (
 	<ul className={css.contact__list}>{contacts.map(({id, name, number}) => <li key = {id} className={css.contact__item}>
 	<p>{name} :</p>
 	<p>{number}</p>
+	<button>Delete</button>
 </li>)}
 </ul>
 )
