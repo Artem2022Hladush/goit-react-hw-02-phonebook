b5a692ac270641f0cc3166c476cbc9b96288d439
Артем Гладуш <m1hommage5008@gmail.com>
2022-12-01 17:51:24 +0200

    add Components ContactList

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import css from "../ContactList/ContactLIst.module.css"
+
+const ContactLIst = ({contacts}) => {
+return (
+	<ul className={css.contact__list}>{contacts.map(({id, name, number}) => <li key = {id} className={css.contact__item}>
+	<p>{name} :</p>
+	<p>{number}</p>
+</li>)}
+</ul>
+)
+};
+
+export default ContactLIst;
