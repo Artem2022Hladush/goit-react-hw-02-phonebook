f4fb163a31a57b8584b83105ebc586b4d6399061
Артем Гладуш <m1hommage5008@gmail.com>
2022-12-12 09:59:27 +0200

    add style Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import css from "../Contact/Contact.module.css"
 
 const Contact = ({name, number, onDeleteContact}) => (
-		<div>
-			<p>{name} :</p>
-			<p>{number}</p>
+		<div className={css.contact}>
+			<p className={css.name}>{name} :</p>
+			<p className={css.number}>{number}</p>
 			<button className={css.contact__button} type="button" onClick={onDeleteContact}>Delete</button>
 		</div>
 );
